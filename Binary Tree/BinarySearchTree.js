2764b1e75790a8cb1b309304408518f5be9ac847
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2022-06-10 15:09:22 +0300

    Update bst - traversal methods - inorder, postorder, preorder and search

--- Binary Tree/BinarySearchTree.js
+++ Binary Tree/BinarySearchTree.js
@@ -98,6 +98,44 @@ class BinarySearchTree {
 
         return nodes;
     }
+
+    inorder(node) {
+        if (node != null) {
+            this.inorder(node.left);
+            console.log(node.data);
+            this.inorder(node.right);
+        }
+    }
+
+    preorder() {
+        if (node != null) {
+            console.log(node.data);
+            this.preorder(node.left);
+            this.preorder(node.right);
+        }
+    }
+
+    postorder() {
+        if (node != null) {
+            this.postorder(node.left);
+            this.postorder(node.right);
+            console.log(node.data);
+        }
+    }
+
+    search(node, data) {
+        if (node == null) {
+            return null;
+        }
+
+        if (data > node.data) {
+            return this.search(node.right, data);
+        } else if (data < node.data) {
+            return this.search(node.left, data);
+        } else {
+            return node;
+        }
+    }
 }
 
 const bst = new BinarySearchTree();
