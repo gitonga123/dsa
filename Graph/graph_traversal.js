672dd2aac83cd1c3d2eebc2d60cfd95b291b093f
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2022-06-13 18:01:57 +0300

    Traverse the graph, both depth wise and breadth wise

---
+++ Graph/graph_traversal.js
@@ -0,0 +1,66 @@
+// processing the node after it leaves the stack.
+// depth, process the height first before traversing on the width;
+
+const depthFirstPrint = (graph, source) => {
+    const stack = [source];
+
+    while (stack.length > 0) {
+        const curr = stack.pop();
+
+        console.log(curr);
+
+        if (graph[curr]) {
+            for (let n of graph[curr]) {
+                stack.push(n);
+            }
+        }
+    }
+}
+
+const depthFirstPrintRecursive = (graph, source) => {
+    console.log(source);
+
+    for (let n of graph[source]) {
+        depthFirstPrintRecursive(graph, n);
+    }
+}
+
+const breadthFirstPrint = (graph, source) => {
+    const queue = [source];
+    while (queue.length > 0) {
+        const curr = queue.shift();
+        console.log(curr);
+        for (let n of graph[curr]) {
+            queue.push(n);
+        }
+    }
+}
+
+const hasPath = (graph, src, dst) => {
+    const stack = [src];
+    while (stack.length > 0) {
+        const curr = stack.pop();
+        if (curr == dst) {
+            return true;
+        }
+
+        for (let n of graph[curr]) {
+            stack.push(n);
+        }
+    }
+    return false
+};
+
+
+const graph = {
+    a: ['b', 'c'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: [],
+    f: []
+};
+
+breadthFirstPrint(graph, 'a');
+depthFirstPrint(graph, 'a');
+depthFirstPrintRecursive(graph, 'a');
