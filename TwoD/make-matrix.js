c0d766e1c687b18f9607797a329210a244f613c5
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2022-06-17 12:53:11 +0300

    Create matrix

---
+++ TwoD/make-matrix.js
@@ -0,0 +1,29 @@
+// Write a function `makeMatrix(m, n, value)` that accepts three arguments. The function should return
+// a 2-dimensional array of height `m` and width `n` that contains the `value` as every element.
+const makeMatrix = (height, width, values) => {
+    const matrix = Array(height).fill([]);
+    for(let i = 0; i < matrix.length; i++) {
+        matrix[i] = Array(width).fill(values);
+    }
+    return matrix;
+}
+console.log(makeMatrix(3, 5, null));
+// [
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ]
+// ]
+
+console.log(makeMatrix(4, 2, "x"));
+// [
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ]
+// ]
+
+console.log(makeMatrix(2, 2, 0));
+// [
+//   [ 0, 0 ],
+//   [ 0, 0 ]
+// ]
