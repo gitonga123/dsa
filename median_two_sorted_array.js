80cb3a89a61c72b09d0e5a7fc80113963d0646a9
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2022-06-28 14:50:01 +0300

    Median of two sorted array

---
+++ median_two_sorted_array.js
@@ -0,0 +1,43 @@
+// 4. Median of Two Sorted Arrays
+/**
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+ * The overall run time complexity should be O(log (m+n)).
+ */
+/**
+ * Input: nums1 = [1,3], nums2 = [2]
+ * Output: 2.00000
+ * Explanation: merged array = [1,2,3] and median is 2.
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+    if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+
+    const x = nums1.length, y = nums2.length;
+    let lo = 0, hi = x;
+
+    while (lo <= hi) {
+        let partitionX = (lo + hi) / 2 | 0,
+            partitionY = (x + y + 1) / 2 - partitionX | 0;
+
+        let maxLeftX = partitionX === 0 ? -Infinity : nums1[partitionX - 1];
+        let minRightX = partitionX === x ? Infinity : nums1[partitionX];
+
+        let maxLeftY = partitionY === 0 ? -Infinity : nums2[partitionY - 1];
+        let minRightY = partitionY === y ? Infinity : nums2[partitionY];
+
+        if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+            if ((x + y) & 1) return Math.max(maxLeftX, maxLeftY);
+            return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+        } else if (maxLeftX > minRightY) {
+            hi = partitionX - 1;
+        } else {
+            lo = partitionX + 1;
+        }
+    }
+};
+
+console.log(findMedianSortedArrays([3], [1,2]));
