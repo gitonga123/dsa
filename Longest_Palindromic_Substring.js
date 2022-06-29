e707b43d8a33b3eba2354ea2604b31b274bdc56a
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2022-06-29 16:45:04 +0300

    Longest Palindrome

---
+++ Longest_Palindromic_Substring.js
@@ -0,0 +1,31 @@
+// ** Given a string s, return the longest palindromic substring in s.
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (str) {
+    if (str.length <= 0) return 0;
+
+    let longest = -Infinity;
+
+    for (let i = 0; i < str.length; i++) {
+        console.log("1--->",str, i, i);
+        let longestOdd = expandFromCenter(str, i, i);
+        let longestEven = expandFromCenter(str, i-1, i);
+        longest = Math.max(longest, longestEven, longestOdd);
+    }
+
+    return longest;
+};
+
+function expandFromCenter(str, left, right) {
+    let i = 0;
+    while (str[left - i] && str[left - i] === str[right + i]) {
+        i++
+    }
+    i--;
+    let string = str.slice(left - 1, right + i + 1);
+    return string.length;
+}
+
+console.log(longestPalindrome('banana'));
