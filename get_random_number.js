d719b612ad28c0678c2d4cfa854f137a0dba224a
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2022-07-05 17:53:21 +0300

    Get random number from range

--- get_random_number.js
+++ get_random_number.js
@@ -3,10 +3,24 @@
 // every number has the same probability
 
 const getRandomNumbers = (llist) => {
-    const minVal = llist[0][0];
-    const maxVal = llist[llist.length - 1][1];
-    return Math.floor(Math.random() * (maxVal - minVal + 1) + minVal);
+    let total_nums = 0;
+
+    for (let index = 0; index < llist.length; index++) {
+        total_nums += llist[index][1] - llist[index][0] + 1;
+    }
+    let random = Math.floor(Math.random() * (total_nums));
+    
+    for (let index = 0; index < llist.length; index++) {
+        let current_range = llist[index][1] - llist[index][0];
+        if (random <= current_range) {
+            return llist[index][0] + random;
+        } else {
+            random = random - current_range;
+        }
+    }
+    return 0;
+
 }
 
-let numbers = [[5, 8], [15, 20], [30, 56], [80, 98]];
+let numbers = [[5, 8], [15, 20], [32, 34]];
 console.log(getRandomNumbers(numbers));
