1f2ffb41abe6cb8180b96c3a45a8b3ada600bed3
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2022-07-06 12:07:16 +0300

    Binary Tree Max Path o leaf

--- Binary Tree/binary_tree_max_path_sum.js
+++ Binary Tree/binary_tree_max_path_sum.js
@@ -1,10 +1,42 @@
+
+class Node {
+    constructor(data, left = null, right = null) {
+        this.val = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+const a = new Node(10);
+const b = new Node(12);
+const c = new Node(5);
+const d = new Node(18);
+const e = new Node(5);
+
+a.left = c;
+a.right = b;
+c.left = e;
+b.right = d;
+
+
+
+//         10
+//     5           12
+// 5                   18
+
 const maxPathSum = (root) => {
     if (root == null) return -Infinity;
-    if (root && root.left === null && root.right == null) return root.val
+
+    if (root && root.left === null && root.right == null) {
+        return root.val
+    }
     const maxPathSumChild = Math.max(
         maxPathSum(root.left),
         maxPathSum(root.right)
     )
-
+    //console.log("max child--->", maxPathSumChild);
     return root.val + maxPathSumChild;
 }
+
+console.log(maxPathSum(a));
