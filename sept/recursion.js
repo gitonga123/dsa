9df9e39c21e85b814866c64e71f85c3426695e88
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2023-09-15 17:23:30 +0300

    Recursion, we will come back later

--- sept/recursion.js
+++ sept/recursion.js
@@ -1,12 +1,29 @@
-function factorial(n) {
-    let value = 1;
+// we can improve using memoizing 
+
+function factorial(n, mem = {}) {
     if (n == 1) return 1;
 
-    if (n == 2) return 2;
-
-    value = n * factorial(n - 1);
+    value = n * factorial(n - 1, mem);
 
     return value;
 }
 
-console.log(factorial(5));
+const calculateSum = (n) => {
+    if (n.length == 0) return 0;
+    const rest = n.slice(1);
+    return n[0] + calculateSum(rest);
+}
+
+const sum = (n) => {
+    return _sum(n, 0);
+}
+
+const _sum = (array, index) => {
+    if (index == array.length) return 0;
+
+    return array[index] + _sum(array, index + 1);
+}
+
+// Time: O(n)
+// space: O(n)
+console.log(sum([5, 1 - 7, 8]));
